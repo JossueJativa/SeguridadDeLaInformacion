c725317d14f402b67204c029a9b11554477e7881
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-12-08 19:50:59 -0500

    Filtracion de departamentos para que no aparezcan muchas veces el mismo

--- ActivosInformacion/InitialPage/static/js/main.js
+++ ActivosInformacion/InitialPage/static/js/main.js
@@ -18,7 +18,65 @@ function toggleCampoAdicional2() {
   campoAdicional.style.display = document.getElementById('state2').checked ? 'block' : 'none';
 }
 
-document.getElementById("inpSearch").addEventListener("input", e => {
+function actualizarCampos() {
+  var valorAsignado = document.getElementById('valorationAssing').value;
+  var campoCualitativo = document.getElementById('cualitative');
+  var campoDescripcion = document.getElementById('descriptionValue');
+
+  // Colocar el valor cualitativo y la descripción según el valor asignado
+  switch (valorAsignado) {
+      case '0':
+          campoCualitativo.value = 'Despreciable';
+          campoDescripcion.value = 'Irrelebante a efectos prácticos';
+          break;
+      case '1':
+          campoCualitativo.value = 'Bajo';
+          campoDescripcion.value = 'Daño menor';
+          break;
+      case '2':
+          campoCualitativo.value = 'Bajo';
+          campoDescripcion.value = 'Daño menor';
+          break;
+      case '3':
+          campoCualitativo.value = 'Medio';
+          campoDescripcion.value = 'Daño importante';
+          break;
+      case '4':
+          campoCualitativo.value = 'Medio';
+          campoDescripcion.value = 'Daño importante';
+          break;
+      case '5':
+          campoCualitativo.value = 'Medio';
+          campoDescripcion.value = 'Daño importante';
+          break;
+      case '6':
+          campoCualitativo.value = 'Alto';
+          campoDescripcion.value = 'Daño grave';
+          break;
+      case '7':
+          campoCualitativo.value = 'Alto';
+          campoDescripcion.value = 'Daño grave';
+          break;
+      case '8':
+          campoCualitativo.value = 'Alto';
+          campoDescripcion.value = 'Daño grave';
+          break;
+      case '9':
+          campoCualitativo.value = 'Muy Alto';
+          campoDescripcion.value = 'Daño muy grave';
+          break;
+      case '10':
+          campoCualitativo.value = 'Extremo';
+          campoDescripcion.value = 'Daño extremadamente grave';
+          break;
+      default:
+          campoCualitativo.value = '';
+          campoDescripcion.value = '';
+  }
+}
+
+try{
+  document.getElementById("inpSearch0").addEventListener("input", e => {
     const searchInput = e.target.value.toLowerCase();
     const tabla = document.getElementById("searchtable");
     const tr = tabla.getElementsByTagName("tr");
@@ -38,51 +96,133 @@ document.getElementById("inpSearch").addEventListener("input", e => {
       }
     }
   });
+}catch{
 
-  try{
-    document.getElementById("inpSearchAsset").addEventListener("input", e => {
-      console.log(e.target.value)
-      const searchInput = e.target.value.toLowerCase();
-      const tabla = document.getElementById("searchtable");
-      const tr = tabla.getElementsByTagName("tr");
-    
-      for (let i = 1; i < tr.length; i++) {
-        const td = tr[i].getElementsByTagName("td")[2];
-    
-        if (td) {
-          const txtValue = td.textContent || td.innerText;
-          const txtValueLowerCase = txtValue.toLowerCase();
-    
-          if (txtValueLowerCase.includes(searchInput)) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
+}
+
+try{
+  document.getElementById("inpSearch1").addEventListener("input", e => {
+    console.log(e.target.value);
+    const searchInput = e.target.value.toLowerCase();
+    const tabla = document.getElementById("searchtable");
+    const tr = tabla.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[1];
+  
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
+        const txtValueLowerCase = txtValue.toLowerCase();
+  
+        if (txtValueLowerCase.includes(searchInput)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
       }
-    });
+    }
+  });
+} catch {
 
-    document.getElementById("inpSearchDepartment").addEventListener("input", e => {
-      console.log(e.target.value)
-      const searchInput = e.target.value.toLowerCase();
-      const tabla = document.getElementById("searchtable");
-      const tr = tabla.getElementsByTagName("tr");
-    
-      for (let i = 1; i < tr.length; i++) {
-        const td = tr[i].getElementsByTagName("td")[4];
-    
-        if (td) {
-          const txtValue = td.textContent || td.innerText;
-          const txtValueLowerCase = txtValue.toLowerCase();
-    
-          if (txtValueLowerCase.includes(searchInput)) {
-            tr[i].style.display = "";
-          } else {
-            tr[i].style.display = "none";
-          }
+}
+
+
+try{
+  document.getElementById("inpSearch2").addEventListener("input", e => {
+    const searchInput = e.target.value.toLowerCase();
+    const tabla = document.getElementById("searchtable");
+    const tr = tabla.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[2];
+  
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
+        const txtValueLowerCase = txtValue.toLowerCase();
+  
+        if (txtValueLowerCase.includes(searchInput)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
         }
       }
-    });
-  }
-  catch(error){
-  }
+    }
+  });
+}catch{
+
+}
+
+try{
+  document.getElementById("inpSearch3").addEventListener("input", e => {
+    const searchInput = e.target.value.toLowerCase();
+    const tabla = document.getElementById("searchtable");
+    const tr = tabla.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[4];
+  
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
+        const txtValueLowerCase = txtValue.toLowerCase();
+  
+        if (txtValueLowerCase.includes(searchInput)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  });
+}catch {
+
+}
+
+try{
+  document.getElementById("inpSearch4").addEventListener("input", e => {
+    const searchInput = e.target.value.toLowerCase();
+    const tabla = document.getElementById("searchtable");
+    const tr = tabla.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[4];
+  
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
+        const txtValueLowerCase = txtValue.toLowerCase();
+  
+        if (txtValueLowerCase.includes(searchInput)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  });
+} catch{
+
+}
+
+try{
+  document.getElementById("inpSearch5").addEventListener("input", e => {
+    const searchInput = e.target.value.toLowerCase();
+    const tabla = document.getElementById("searchtable");
+    const tr = tabla.getElementsByTagName("tr");
+  
+    for (let i = 1; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[5];
+  
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
+        const txtValueLowerCase = txtValue.toLowerCase();
+  
+        if (txtValueLowerCase.includes(searchInput)) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  });
+}catch{
+
+}
--- ActivosInformacion/InitialPage/views.py
+++ ActivosInformacion/InitialPage/views.py
@@ -8,12 +8,217 @@ from Users.models import User, Workload
 # Create your views here.
 def enterAsset(request):
     if request.user.is_authenticated:
-        return render(request, "home/enterAsset.html")
+        # Obtener todos los departamentos sin nombres duplicados
+        departmentsName = Departments.objects.values('name').distinct()
+
+        # Crear id para cada departamento
+        for department in departmentsName:
+            department['id'] = Departments.objects.filter(name=department['name']).first().id
+
+        if request.method == "POST":
+            # Informacion inicial
+            origin = request.POST.get("origin")
+            code = request.POST.get("code")
+            name = request.POST.get("name")
+            type = request.POST.get("type")
+            subtype = request.POST.get("subtype")
+            responsableArea = request.POST.get("responsableArea")
+            responsablePerson = request.POST.get("responsablePerson")
+            ubicationType = request.POST.get("ubicationType")
+            ubication = request.POST.get("ubication")
+            quantity = request.POST.get("quantity")
+            characteristic = request.POST.get("characteristic")
+
+            # Verificacion de toggles
+            state = request.POST.get("state")
+            state2 = request.POST.get("state2")
+            
+            if state == "on":
+                #Informacion de dependencias
+                dependency = request.POST.get("dependency")
+                descriptionDependency = request.POST.get("descriptionDependency")
+                responsableUser = request.POST.get("responsableUser")
+                dependsOf = request.POST.get("dependsOf")
+
+                if dependency == "" or dependency == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle
+                        "state": state,
+                        "descriptionDependency": descriptionDependency,
+                        "responsableUser": responsableUser,
+                        "dependsOf": dependsOf,
+                        "message": "Seleccione la dependencia",
+                    })
+                
+                if descriptionDependency == "" or descriptionDependency == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle
+                        "state": state,
+                        "dependency": dependency,
+                        "responsableUser": responsableUser,
+                        "dependsOf": dependsOf,
+                        "message": "Ingrese la descripción de la dependencia",
+                    })
+                
+                if responsableUser == "" or responsableUser == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle
+                        "state": state,
+                        "dependency": dependency,
+                        "descriptionDependency": descriptionDependency,
+                        "dependsOf": dependsOf,
+                        "message": "Seleccione el responsable del activo"
+                    })
+                
+                if dependsOf == "" or dependsOf == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle 1
+                        "state": state,
+                        "dependency": dependency,
+                        "descriptionDependency": descriptionDependency,
+                        "responsableUser": responsableUser,
+                        # Activacion del toggle 2
+                        "state2": state2,
+                        "valorationDimention": valorationDimention,
+                        "valorationAssing": valorationAssing,
+                        "cualitative": cualitative,
+                        "descriptionValue": descriptionValue,
+                        "message": "Seleccione de que depende"
+                    })
+
+            if state2 == "on":
+                # Informacion de valoracion
+                valorationDimention = request.POST.get("valorationDimention")
+                valorationAssing = request.POST.get("valorationAssing")
+                cualitative = request.POST.get("cualitative")
+                descriptionValue = request.POST.get("descriptionValue")
+                
+                if valorationDimention == "" or valorationDimention == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle 1
+                        "state": state,
+                        "dependency": dependency,
+                        "descriptionDependency": descriptionDependency,
+                        "responsableUser": responsableUser,
+                        "dependsOf": dependsOf,
+                        # Activacion del toggle 2
+                        "state2": state2,
+                        "valorationAssing": valorationAssing,
+                        "cualitative": cualitative,
+                        "descriptionValue": descriptionValue,
+                        "message": "Seleccione la dimensión de valoración"
+                    })
+                
+                if valorationAssing == "" or valorationAssing == None:
+                    return render(request, "home/enterAsset.html",{
+                        "Departments": departmentsName,
+                        # Parte principal
+                        "origin": origin,
+                        "code": code,
+                        "name": name,
+                        "type": type,
+                        "subtype": subtype,
+                        "responsableArea": responsableArea,
+                        "responsablePerson": responsablePerson,
+                        "ubicationType": ubicationType,
+                        "ubication": ubication,
+                        "quantity": quantity,
+                        "characteristic": characteristic,
+                        # Activacion del toggle 1
+                        "state": state,
+                        "dependency": dependency,
+                        "descriptionDependency": descriptionDependency,
+                        "responsableUser": responsableUser,
+                        "dependsOf": dependsOf,
+                        # Activacion del toggle 2
+                        "state2": state2,
+                        "valorationDimention": valorationDimention,
+                        "cualitative": cualitative,
+                        "descriptionValue": descriptionValue,
+                        "message": "Seleccione la valoración"
+                    })
+        else:
+            return render(request, "home/enterAsset.html",{
+                "Departments": departmentsName,
+                "Users": User.objects.all()
+            })
     else:
         return HttpResponseRedirect(reverse("user:login_view"))
 
 def enterUsers(request):
     if request.user.is_authenticated:
+        # Obtener todos los departamentos sin nombres duplicados
+        departmentsName = Departments.objects.values('name').distinct()
+
+        # Crear id para cada departamento
+        for department in departmentsName:
+            department['id'] = Departments.objects.filter(name=department['name']).first().id
+
         if request.method == "POST":
             firstname = request.POST.get("firstname")
             lastname = request.POST.get("lastname")
@@ -27,63 +232,63 @@ def enterUsers(request):
             if firstname == "" or firstname == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Ingrese el nombre del usuario"
                 })
             
             if lastname == "" or lastname == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Ingrese el apellido del usuario"
                 })
             
             if email == "" or email == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Ingrese el correo del usuario"
                 })
             
             if celular == "" or celular == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Ingrese la identidad del usuario"
                 })
             
             if password == "" or password == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Ingrese la contraseña del usuario"
                 })
             
             if confirm == "" or confirm == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Confirme la contraseña del usuario"
                 })
             
             if userDepartment == "" or userDepartment == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Seleccione el departamento del usuario"
                 })
             
             if workload == "" or workload == None:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Seleccione la carga de trabajo del usuario"
                 })
             
             if password != confirm:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": "Las contraseñas no coinciden"
                 })
             
@@ -100,7 +305,7 @@ def enterUsers(request):
             except Exception as e:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": f"Error al ingresar el departamento {e}"
                 })
             
@@ -125,7 +330,7 @@ def enterUsers(request):
             except Exception as e:
                 return render(request, "home/enterUsers.html",{
                     "Workloads": Workload.objects.all(),
-                    "Departments": Departments.objects.all(),
+                    "Departments": departmentsName,
                     "message": f"Error al ingresar el usuario {e}"
                 })
             
@@ -134,7 +339,7 @@ def enterUsers(request):
             })
         return render(request, "home/enterUsers.html",{
             "Workloads": Workload.objects.all(),
-            "Departments": Departments.objects.all()
+            "Departments": departmentsName
         })
     else:
         return HttpResponseRedirect(reverse("user:login_view"))
