d3f3c99cdd7853782fb8eb52e40267fe732380e1
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-12-12 14:35:42 -0500

    Imgreso del administrador

--- ActivosInformacion/InitialPage/admin.py
+++ ActivosInformacion/InitialPage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from InitialPage.models import TypeAssets, SubtypeAssets, Departments, Assets, AssetsDependence, AssetsValue
 
 # Register your models here.
+admin.site.register(TypeAssets)
+admin.site.register(SubtypeAssets)
+admin.site.register(Departments)
+admin.site.register(Assets)
+admin.site.register(AssetsDependence)
+admin.site.register(AssetsValue)
--- ActivosInformacion/Users/admin.py
+++ ActivosInformacion/Users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from Users.models import User, Workload
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Workload)
